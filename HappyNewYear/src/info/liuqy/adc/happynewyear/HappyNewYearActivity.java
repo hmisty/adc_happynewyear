ff780bb99ec0c35d66cd444b1335ab439dc98c8c
Evan Liu <qingyan123@gmail.com>
2012-04-30 22:57:15 +0800

    newly created.

---
+++ HappyNewYear/src/info/liuqy/adc/happynewyear/HappyNewYearActivity.java
@@ -0,0 +1,13 @@
+package info.liuqy.adc.happynewyear;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class HappyNewYearActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
