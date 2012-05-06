ac7a92cb510700635e4bb00f5a481ca29ab99c7e
Evan Liu <qingyan123@gmail.com>
2012-05-06 23:58:12 +0800

    implement genSendList()

--- HappyNewYear/src/info/liuqy/adc/happynewyear/HappyNewYearActivity.java
+++ HappyNewYear/src/info/liuqy/adc/happynewyear/HappyNewYearActivity.java
@@ -6,6 +6,7 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
@@ -14,7 +15,10 @@ import android.provider.ContactsContract.CommonDataKinds.Note;
 import android.provider.ContactsContract.CommonDataKinds.Phone;
 import android.provider.ContactsContract.Contacts;
 import android.provider.ContactsContract.Data;
-import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 
 public class HappyNewYearActivity extends Activity {
 	enum Market {
@@ -195,5 +199,33 @@ public class HappyNewYearActivity extends Activity {
 		
 		return false;
 	}
+
+    public static final String SENDLIST = "info.liuqy.adc.happynewyear.SENDLIST";
+    public static final String CUSTOMER_CARER = "info.liuqy.adc.happynewyear.CUSTOMER_CARER";
+    public static final String SMS_TEMPLATE = "info.liuqy.adc.happynewyear.SMS_TEMPLATE";   
+
+    public void genSendlist(View v) {
+        RadioGroup rg = (RadioGroup)this.findViewById(R.id.customer_group);
+        int id = rg.getCheckedRadioButtonId();
+        Market targetMarket = (id == R.id.btn_north) ? Market.NORTH : Market.SOUTH;
+
+        rg = (RadioGroup)this.findViewById(R.id.customer_lang);
+        id = rg.getCheckedRadioButtonId();
+        Language targetLanguage = (id == R.id.btn_cn) ? Language.CHINESE : Language.ENGLISH;
+
+        Spinner sp = (Spinner)this.findViewById(R.id.customer_carer);
+        String cc = sp.getSelectedItem().toString();
+        
+        EditText et = (EditText)this.findViewById(R.id.sms_template);
+        String tmpl = et.getText().toString();
+        
+        Bundle sendlist = readContacts(targetMarket, targetLanguage);
+        
+        Intent i = new Intent(this, SendListActivity.class);
+        i.putExtra(SENDLIST, sendlist);
+        i.putExtra(CUSTOMER_CARER, cc);
+        i.putExtra(SMS_TEMPLATE, tmpl);
+        startActivity(i);
+    }
 
 }
