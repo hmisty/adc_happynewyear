616a57af32dcc219252b7a44de7645d0db1309bf
Evan Liu <qingyan123@gmail.com>
2012-05-06 23:55:46 +0800

    newly created SendListActivity.

---
+++ HappyNewYear/src/info/liuqy/adc/happynewyear/SendListActivity.java
@@ -0,0 +1,14 @@
+package info.liuqy.adc.happynewyear;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+
+public class SendListActivity extends ListActivity {
+
+	@Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sendlist);
+    }
+
+}
