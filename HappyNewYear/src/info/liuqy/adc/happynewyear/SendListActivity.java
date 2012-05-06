79f192561873d2ef490844df135a9ecdab62c496
Evan Liu <qingyan123@gmail.com>
2012-05-07 00:00:12 +0800

    SendListActivity: set adapter. to implement handleIntent().

--- HappyNewYear/src/info/liuqy/adc/happynewyear/SendListActivity.java
+++ HappyNewYear/src/info/liuqy/adc/happynewyear/SendListActivity.java
@@ -1,14 +1,37 @@
 package info.liuqy.adc.happynewyear;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.SimpleAdapter;
 
 public class SendListActivity extends ListActivity {
+	
+    static final String KEY_TO = "TO";
+    static final String KEY_SMS = "SMS";
+    
+    //[<TO, number>,<SMS, sms>]
+    List<Map<String, String>> smslist = new LinkedList<Map<String, String>>();
+    SimpleAdapter adapter;
 
 	@Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.sendlist);
+        
+        adapter = new SimpleAdapter(this, smslist,
+                android.R.layout.simple_list_item_2,
+                new String[]{KEY_TO, KEY_SMS},
+                new int[]{android.R.id.text1, android.R.id.text2});
+        this.setListAdapter(adapter);
+        handleIntent();
     }
+	
+	public void handleIntent() {
+	
+	}
 
 }
